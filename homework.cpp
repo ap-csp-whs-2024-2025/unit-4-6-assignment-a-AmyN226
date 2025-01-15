74e17ec40999b48a1182ff1f01eab212bfea2aad
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-01-15 21:06:16 +0000

    finished

--- homework.cpp
+++ homework.cpp
@@ -10,6 +10,7 @@
 #include <iostream>  // std::cin, std::cout
 #include <string>    // std::string, std::getline
 #include <vector>    // std::vector
+#include <cmath>
 
 
 
@@ -17,22 +18,53 @@ void compare(int n1, int n2)
 {
     if (n1 > n2)
     {
-        std::cout << n1 << "is greater than " << n2 << std::endl;
+        std::cout << n1 << " is greater than " << n2 << std::endl;
     }
     else
-    if 
+    if (n1 < n2)
+    {
+         std::cout << n1 << " is less than " << n2 << std::endl;
+    }
+}
+
+void quadForm(int a, int b, int c)
+{
+
+ std::cout << "The answers are " << (((-1 * b ) + sqrt(b*b - 4*a*c))/(2 * a)) << " and " << (((-1 * b ) - sqrt(b*b - 4*a*c))/(2 * a)) << std::endl;
+
+}
+
+int curr_index = 0;
+int min = listA[0];
+void minList(listA)
+{
+ while (curr_index < numbers.size())
+{
+
+  if (listA[curr_index] < min)
+
+  {
+
+     min = listA[curr_index];
+
+  }
+
+  curr_index = curr_index + 1;
+}
+ std::cout << "The minimum is " << min << std::endl;
 }
 
 int main()
 {
-    // Complete your homework here
- //task 1
 
 
+ compare(6, 5);
+ compare (2, 7);
 
+ quadForm(1, -5, 6);
+ quadForm(1, 9, 14);
 
-
-
+minlist(listA)
 
 
 
